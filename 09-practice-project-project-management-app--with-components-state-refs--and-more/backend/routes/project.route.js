cab4302ada35620e2a456ca78f79577ee61bf1fc
Doktorlich <sergei199522@mail.ru>
2025-08-13 00:32:03 +0300

    Module "09  Working with Refs & Portals"     - Minor change route

--- 09-practice-project-project-management-app--with-components-state-refs--and-more/backend/routes/project.route.js
+++ 09-practice-project-project-management-app--with-components-state-refs--and-more/backend/routes/project.route.js
@@ -1,27 +1,28 @@
 import { Router } from "express";
 import { projectController } from "../controllers/project.controller.js";
-import {validator} from "../middleware/validation.middleware.js";
+import { validator } from "../middleware/validation.middleware.js";
 
 const router = Router();
-const [validationTitle,validationDescription,validationDueDate] = [validator.validationProject.title,validator.validationProject.description,validator.validationProject.dueDate]
+const [validationTitle, validationDescription, validationDueDate] = [
+    validator.validationProject.title,
+    validator.validationProject.description,
+    validator.validationProject.dueDate,
+];
 //начальная страница +
-router.get("api/", projectController.getHome);
+router.get("/", projectController.getHome);
 //страница создания нового проекта+
-router.get("create-project", projectController.getCreateProject);
+router.get("/create-project", projectController.getCreateProject);
 // пост запрос на создание нового проекта+
-router.post("create-project/create",validationTitle,validationDescription,validationDueDate, projectController.postCreateProject);
+router.post("/create-project/create", validationTitle, validationDescription, validationDueDate, projectController.postCreateProject);
 // get запрос для просмотра конкретного проекта+
-router.get("project/:project", projectController.getProject);
+router.get("/project/:project", projectController.getProject);
 
 // пост запрос на удаление проекта+
-router.delete("project/:project/delete", projectController.deleteProject);
-
-
-
+router.delete("/project/:project/delete", projectController.deleteProject);
 
 //пост запрос на создание задачи
-router.post("project/:project/create-task",validator.validationTask.text, projectController.postCreateTask);
+router.post("/project/:project/create-task", validator.validationTask.text, projectController.postCreateTask);
 // пост запрос на удаление задачи
-router.delete("project/:project/:task/delete-task", projectController.deleteTask);
+router.delete("/project/:project/:task/delete-task", projectController.deleteTask);
 
 export default router;
--- 09-practice-project-project-management-app--with-components-state-refs--and-more/backend/server.js
+++ 09-practice-project-project-management-app--with-components-state-refs--and-more/backend/server.js
@@ -4,19 +4,11 @@ dotenv.config();
 import cors from "cors";
 import mongoose from "mongoose";
 import routeIndex from "./routes/index.route.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const server = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Теперь можно использовать __dirname
-
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-// server.use(express.static(path.join(__dirname, "public")));
 
 server.use(
     cors({
@@ -26,11 +18,6 @@ server.use(
     }),
 );
 
-// server.use((req, res, next) => {
-//     console.log(`Incoming request: ${req.method} ${req.url}`);
-//     next();
-// });
-
 server.use(routeIndex);
 server.use((error, req, res, next) => {
     console.log(error);
@@ -39,10 +26,6 @@ server.use((error, req, res, next) => {
     const data = error.data;
     res.status(status).json({ message: message, data: data });
 });
-
-// server.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
 
 mongoose
     .connect(`${process.env.MONGODB_URI}`)
