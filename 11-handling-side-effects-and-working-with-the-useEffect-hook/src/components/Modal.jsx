d2d00de49ae0785192c18187a5c94db6c367a7ab
Doktorlich <sergei199522@mail.ru>
2025-08-26 01:53:18 +0300

    Module "11  Handling Side Effects & Working with the useEffect() Hook"        - Add module

---
+++ 11-handling-side-effects-and-working-with-the-useEffect-hook/src/components/Modal.jsx
@@ -0,0 +1,22 @@
+import { forwardRef, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+function Modal({ openModal, children }) {
+    const dialog = useRef();
+    useEffect(() => {
+        if (openModal) {
+            dialog.current.showModal();
+        } else {
+            dialog.current.close();
+        }
+    }, [openModal]);
+
+    return createPortal(
+        <dialog className="modal" ref={dialog} >
+            {openModal ? children : null}
+        </dialog>,
+        document.getElementById("modal"),
+    );
+}
+
+export default Modal;
