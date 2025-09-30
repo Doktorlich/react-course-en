53f46916e7157258ba11e246c4310c3ec54c38bd
Doktorlich <sergei199522@mail.ru>
2025-09-30 17:11:46 +0300

    Module "15  Sending Http Requests (e.g  Connecting to a Database)"        - end module.

--- 15-sending-http-requests--eg-connecting-to-a-database/src/App.jsx
+++ 15-sending-http-requests--eg-connecting-to-a-database/src/App.jsx
@@ -1,76 +1,130 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from "react";
 
-import Places from './components/Places.jsx';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import AvailablePlaces from './components/AvailablePlaces.jsx';
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import { fetchUserPlaces, updateUserPlaces } from "./http.js";
+import Error from "./components/Error.jsx";
 
 function App() {
-  const selectedPlace = useRef();
+    const selectedPlace = useRef();
 
-  const [userPlaces, setUserPlaces] = useState([]);
+    const [userPlaces, setUserPlaces] = useState([]);
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState(null);
+    //состояние которое позволяет сказать пользователю, что нужно подождать прежде чем данные будут получены от сервера
+    const [isFetching, setIsFetching] = useState(false);
+    //состояние в котором храним полученные статус ошибки
+    const [error, setError] = useState(null);
 
-  function handleStartRemovePlace(place) {
-    setModalIsOpen(true);
-    selectedPlace.current = place;
-  }
+    useEffect(() => {
+        setIsFetching(true);
+        async function fetchPlaces() {
+            try {
+                const selectedUserPlaces = await fetchUserPlaces();
+                console.log("selectedUserPlaces", selectedUserPlaces);
+                setUserPlaces(selectedUserPlaces);
+            } catch (error) {
+                setError({ message: error.message || "Failed to fetch user places." });
+            }
+            setIsFetching(false);
+        }
 
-  function handleStopRemovePlace() {
-    setModalIsOpen(false);
-  }
+        fetchPlaces();
+    }, []);
 
-  function handleSelectPlace(selectedPlace) {
-    setUserPlaces((prevPickedPlaces) => {
-      if (!prevPickedPlaces) {
-        prevPickedPlaces = [];
-      }
-      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
-        return prevPickedPlaces;
-      }
-      return [selectedPlace, ...prevPickedPlaces];
-    });
-  }
+    function handleStartRemovePlace(place) {
+        setModalIsOpen(true);
+        selectedPlace.current = place;
+    }
 
-  const handleRemovePlace = useCallback(async function handleRemovePlace() {
-    setUserPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
+    function handleStopRemovePlace() {
+        setModalIsOpen(false);
+    }
+
+    async function handleSelectPlace(selectedPlace) {
+        setUserPlaces(prevPickedPlaces => {
+            if (!prevPickedPlaces) {
+                prevPickedPlaces = [];
+            }
+            if (prevPickedPlaces.some(place => place.id === selectedPlace.id)) {
+                return prevPickedPlaces;
+            }
+            return [selectedPlace, ...prevPickedPlaces];
+        });
+        console.log("userPlaces", userPlaces);
+        try {
+            await updateUserPlaces([selectedPlace, ...userPlaces]);
+        } catch (error) {
+            setUserPlaces(userPlaces);
+            setErrorUpdatingPlaces({ message: error.message || "Failed to updated places." });
+        }
+    }
+
+    const handleRemovePlace = useCallback(
+        async function handleRemovePlace() {
+            setUserPlaces(prevPickedPlaces =>
+                prevPickedPlaces.filter(place => place.id !== selectedPlace.current.id),
+            );
+            try {
+                await updateUserPlaces(
+                    userPlaces.filter(place => place.id !== selectedPlace.current.id),
+                );
+            } catch (error) {
+                console.error("Error", error);
+                setUserPlaces(userPlaces);
+                setErrorUpdatingPlaces({ message: error.message || "Failed to delete places." });
+            }
+
+            setModalIsOpen(false);
+        },
+        [userPlaces],
     );
 
-    setModalIsOpen(false);
-  }, []);
+    function handleError() {
+        setErrorUpdatingPlaces(null);
+    }
 
-  return (
-    <>
-      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
+    return (
+        <>
+            <Modal open={errorUpdatingPlaces} onClose={handleError}>
+                {errorUpdatingPlaces && (
+                    <Error title={"An error occurred!"} message={errorUpdatingPlaces.message} />
+                )}
+            </Modal>
+            <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+                <DeleteConfirmation
+                    onCancel={handleStopRemovePlace}
+                    onConfirm={handleRemovePlace}
+                />
+            </Modal>
 
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
-      <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
+            <header>
+                <img src={logoImg} alt="Stylized globe" />
+                <h1>PlacePicker</h1>
+                <p>
+                    Create your personal collection of places you would like to visit or you have
+                    visited.
+                </p>
+            </header>
+            <main>
+                {error && <Error title={"An error occurred"} message={error.message}/>}
+                {!error &&<Places
+                    title="I'd like to visit ..."
+                    fallbackText="Select the places you would like to visit below."
+                    places={userPlaces}
+                    loadingText={"Fetching your places..."}
+                    isLoading={isFetching}
+                    onSelectPlace={handleStartRemovePlace}
+                />}
 
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
-      </main>
-    </>
-  );
+                <AvailablePlaces onSelectPlace={handleSelectPlace} />
+            </main>
+        </>
+    );
 }
 
 export default App;
--- 15-sending-http-requests--eg-connecting-to-a-database/src/components/AvailablePlaces.jsx
+++ 15-sending-http-requests--eg-connecting-to-a-database/src/components/AvailablePlaces.jsx
@@ -1,12 +1,54 @@
-import Places from './Places.jsx';
+import Places from "./Places.jsx";
+import { useEffect, useState } from "react";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+    //состояние в котором храним данные полученные от сервера
+    const [availablePlaces, setAvailablePlaces] = useState([]);
+    //состояние которое позволяет сказать пользователю, что нужно подождать прежде чем данные будут получены от сервера
+    const [isFetching, setIsFetching] = useState(false);
+    //состояние в котором храним полученные статус ошибки
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setIsFetching(true);
+        async function fetchPlaces() {
+            setError(null);
+            try {
+                const fetchPlaces = await fetchAvailablePlaces();
+                navigator.geolocation.getCurrentPosition(position => {
+                    const sortedPlaces = sortPlacesByDistance(
+                        fetchPlaces,
+                        position.coords.latitude,
+                        position.coords.longitude,
+                    );
+                    setAvailablePlaces(sortedPlaces);
+                    setIsFetching(false);
+                });
+            } catch (error) {
+                setError({
+                    message: error.message || "Could not fetch places, please try again later.",
+                });
+                setIsFetching(false);
+            }
+        }
+        fetchPlaces();
+    }, []);
+
+    if (error) {
+        return <Error title={"An error occurred!"} message={error.message} />;
+    }
+
+    return (
+        <Places
+            title="Available Places"
+            places={availablePlaces}
+            isLoading={isFetching}
+            loadingText={"Fetching place data..."}
+            fallbackText="No places available."
+            onSelectPlace={onSelectPlace}
+        />
+    );
 }
