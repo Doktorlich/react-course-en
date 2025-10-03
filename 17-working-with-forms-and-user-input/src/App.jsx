0fe48afab34781199904ed01811e6e7d56bb05b4
Doktorlich <sergei199522@mail.ru>
2025-10-03 22:09:44 +0300

    Module "17  Working with Forms & User Input"        - 04 Managing & Getting User Input via State & Generic Handlers        - 05 Getting User Input via Refs - Copy

--- 17-working-with-forms-and-user-input/src/App.jsx
+++ 17-working-with-forms-and-user-input/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header.jsx";
-import Login from "./components/Login.jsx";
+import RefLogin from "./components/RefLogin.jsx";
+import StateLogin from "./components/StateLogin.jsx";
 
 function App() {
     return (
         <>
             <Header />
             <main>
-                <Login />
+                <StateLogin />
+                <RefLogin />
             </main>
         </>
     );
---
+++ 17-working-with-forms-and-user-input/src/components/StateLogin.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function StateLogin() {
+
+    const [enteredValues, setEnteredValues] = useState({
+        email: "",
+        password: "",
+    });
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        console.log("Name: " + name);
+        console.log("Value: " + value);
+        setEnteredValues(prevState => ({ ...prevState, [name]: value }));
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("Entered Email: " + enteredValues.email);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>State Login</h2>
+
+            <div className="control-row">
+                <div className="control no-margin">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        id="email"
+                        type="email"
+                        name="email"
+                        onChange={handleInputChange}
+                        value={enteredValues.email}
+                    />
+                </div>
+
+                <div className="control no-margin">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        onChange={handleInputChange}
+                        value={enteredValues.password}
+                    />
+                </div>
+            </div>
+
+            <p className="form-actions">
+                <button className="button button-flat">Reset</button>
+                <button className="button">Login</button>
+            </p>
+        </form>
+    );
+}
