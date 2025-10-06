81d311b56a414c5761a1c9b95e476e2f3e1347c7
Doktorlich <sergei199522@mail.ru>
2025-10-06 20:08:15 +0300

    Module "17  Working with Forms & User Input"        - 06 Getting Values via FormData & Native Browser APIs        - 07 Resetting Forms        - 08 Validating Input on Every Keystroke via State        - 09 Validating Input Upon Lost Focus (Blur)

--- 17-working-with-forms-and-user-input/src/App.jsx
+++ 17-working-with-forms-and-user-input/src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header.jsx";
 import RefLogin from "./components/RefLogin.jsx";
 import StateLogin from "./components/StateLogin.jsx";
+import Signup from "./components/Signup.jsx";
 
 function App() {
     return (
         <>
             <Header />
             <main>
+                {/*<Signup />*/}
                 <StateLogin />
-                <RefLogin />
+                {/*<RefLogin />*/}
             </main>
         </>
     );
--- 17-working-with-forms-and-user-input/src/components/StateLogin.jsx
+++ 17-working-with-forms-and-user-input/src/components/StateLogin.jsx
@@ -1,22 +1,38 @@
 import { useState } from "react";
 
 export default function StateLogin() {
-
     const [enteredValues, setEnteredValues] = useState({
         email: "",
         password: "",
     });
+
+    const [didEdit, setDidEdit] = useState({
+        email: false,
+        password: false,
+    });
+
+    const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
     function handleInputChange(event) {
         const { name, value } = event.target;
         console.log("Name: " + name);
         console.log("Value: " + value);
         setEnteredValues(prevState => ({ ...prevState, [name]: value }));
+        setDidEdit(prevState => {
+            return { ...prevState, [name]: false };
+        });
     }
 
     function handleSubmit(event) {
         event.preventDefault();
         console.log("Entered Email: " + enteredValues.email);
+    }
+
+    function handleInputBlur(event) {
+        const { name } = event.target;
+        setDidEdit(prevState => {
+            return { ...prevState, [name]: true };
+        });
     }
 
     return (
@@ -30,9 +46,15 @@ export default function StateLogin() {
                         id="email"
                         type="email"
                         name="email"
+                        onBlur={event => {
+                            handleInputBlur(event);
+                        }}
                         onChange={handleInputChange}
                         value={enteredValues.email}
                     />
+                    <div className={"control-error"}>
+                        {emailIsInvalid && <p>Please enter a valid email address</p>}
+                    </div>
                 </div>
 
                 <div className="control no-margin">
