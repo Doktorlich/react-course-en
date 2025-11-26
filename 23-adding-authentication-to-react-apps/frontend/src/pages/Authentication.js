557dc6d34b9adbe945d3e4f05ce488b7d1129822
Doktorlich <sergei199522@mail.ru>
2025-11-26 20:01:13 +0300

    Module "23  Adding Authentication To React Apps"     - 01 Module Introduction     - 02 How Authentication Works     - 03 Project Setup & Route Setup     - 04 Working with Query Parameters     - 05 Implementing the Auth Action     - 06 Validating User Input & Outputting Validation Errors     - 07 Adding User Login     - 08 Attaching Auth Tokens to Outgoing Requests     - 09 Adding User Logout     - 10 Updating the UI Based on Auth Status     - 11 Important loader()s must return null or any other value     - 12 Adding Route Protection     - 13 Adding Automatic Logout     - 14 Managing the Token Expiration

--- 23-adding-authentication-to-react-apps/frontend/src/pages/Authentication.js
+++ 23-adding-authentication-to-react-apps/frontend/src/pages/Authentication.js
@@ -1,7 +1,50 @@
-import AuthForm from '../components/AuthForm';
+import AuthForm from "../components/AuthForm";
+import PageContent from "../components/PageContent";
+import { redirect } from "react-router-dom";
 
 function AuthenticationPage() {
-  return <AuthForm />;
+    return (
+        <PageContent>
+            <AuthForm />
+        </PageContent>
+    );
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+    const url = new URL(request.url);
+    const mode = url.searchParams.get("mode");
+
+    if (mode !== "login" && mode !== "signup") {
+        throw new Response(JSON.stringify({ message: "Unsupported mode." }), { status: 422 });
+    }
+
+    const formData = await request.formData();
+    const data = {
+        email: formData.get("email"),
+        password: formData.get("password"),
+    };
+
+    const response = await fetch(`http://localhost:8080/${mode}`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+    });
+    if (response.status === 422 || response.status === 401) {
+        return response;
+    }
+    if (!response.ok) {
+        throw new Response(JSON.stringify({ message: "Could not authenticate user." }), {
+            status: 500,
+        });
+    }
+
+    const resData = await response.json();
+    const token = resData.token;
+    localStorage.setItem("token", token);
+    const expiration = new Date();
+    expiration.setHours(expiration.getHours() + 1);
+    localStorage.setItem("expiration", expiration.toISOString());
+    return redirect(mode === "login" ? "/" : `/auth?mode=login`);
+}
